de39ba0f2d2cf1b8d334630c00543124e3eb929c
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-03 10:56:15 -0300

    Exercicio 2

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -5,15 +5,31 @@ class Pokedex extends React.Component {
     constructor() {
         super();
         this.clickButton = this.clickButton.bind(this);
+        this.pokemonsFiltreds = this.pokemonsFiltreds.bind(this);
+        this.filterState = this.filterState.bind(this);
+        
         this.state = {
           count: 0,
+          type: 'Fire'
         };
       }
 
+    pokemonsFiltreds(){
+        return this.props.pokemons.filter((pokemon)=> {
+            return pokemon.type === this.state.type;
+        })
+    }
 
-
+    filterState({target}) {
+        const { name } = target;
+        this.setState((_estadoAnterior, _props) => ({
+            type: name,
+          }))
+    }  
+    
       clickButton() {
-        const length = this.props.pokemons.length;
+        this.pokemonsFiltreds()
+        const length = this.pokemonsFiltreds().length;
        
         if (this.state.count < length -1){
         this.setState((estadoAnterior, _props) => ({
@@ -24,18 +40,22 @@ class Pokedex extends React.Component {
                 count: 0,
               }))
         }
-        
-
       }
 
     render() {
         return (
             <div>
                 <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)[this.state.count]}
+                {this.pokemonsFiltreds().map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)[this.state.count]}
                 </div>
                 <div>
-                    <button onClick={this.clickButton}>lalala</button>
+                    <button onClick={this.clickButton}>Próximo Pokemon</button>
+                    <button onClick={this.filterState} name="Fire">Fire</button>
+                    <button onClick={this.filterState} name="Psychic">Psychic</button>
+                    <button onClick={this.filterState} name="Electric">Electric</button>
+                    <button onClick={this.filterState} name="Poison">Poison</button>
+                    <button onClick={this.filterState} name="Normal">Normal</button>
+                    <button onClick={this.filterState} name="Dragon">Dragon</button>
                 </div>
             </div>
         );
