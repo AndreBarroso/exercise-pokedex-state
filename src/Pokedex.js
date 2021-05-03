d6800054bc8b2691006a4ff1419ff8be40cb7e33
AndreBarroso <andre_barroso17@hotmail.com>
2021-05-03 09:35:21 -0300

    Exercicio 1

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,41 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+        this.clickButton = this.clickButton.bind(this);
+        this.state = {
+          count: 0,
+        };
+      }
+
+
+
+      clickButton() {
+        const length = this.props.pokemons.length;
+       
+        if (this.state.count < length -1){
+        this.setState((estadoAnterior, _props) => ({
+            count: estadoAnterior.count + 1,
+          }))
+        } else {
+            this.setState((_estadoAnterior, _props) => ({
+                count: 0,
+              }))
+        }
+        
+
+      }
+
     render() {
         return (
-            <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+            <div>
+                <div className="pokedex">
+                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)[this.state.count]}
+                </div>
+                <div>
+                    <button onClick={this.clickButton}>lalala</button>
+                </div>
             </div>
         );
     }
